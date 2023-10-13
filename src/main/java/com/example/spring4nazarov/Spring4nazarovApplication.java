122dc2863eb262e29791cfc7e6b1239ea0971acd
Alex Nazarov <s10081975s@gmail.com>
2023-10-13 19:41:29 +0000

    Add Spring Boot

---
+++ src/main/java/com/example/spring4nazarov/Spring4nazarovApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring4nazarov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring4nazarovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring4nazarovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring4nazarov/Spring4nazarovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring4nazarov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring4nazarovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
