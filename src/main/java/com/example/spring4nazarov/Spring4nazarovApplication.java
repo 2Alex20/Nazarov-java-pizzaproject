7244e9838aad22932a2f83dc629d1f9dbea2b9cd
2Alex <146747378+2Alex20@users.noreply.github.com>
2023-10-28 02:26:46 +0200

    selecting id

--- src/main/java/com/example/spring4nazarov/Spring4nazarovApplication.java
+++ src/main/java/com/example/spring4nazarov/Spring4nazarovApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +15,31 @@ public class Spring4nazarovApplication {
     public static void main(String[] args) {
       SpringApplication.run(Spring4nazarovApplication.class, args);
     }
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello %s!", name);
+
+    @GetMapping("/cafes")
+    public String cafes() {
+      return "select * from cafe";
     }
+
+    @GetMapping("/cafe/full/{id}") // http://localhost:8080/cafe/full/13 
+    public String getCafe(@PathVariable String cafeid) {
+        return String.format("cafe id: %s!", id);
+    }
+
+
+    @GetMapping("/cafe/full/")
+    public String cafe(@RequestParam(value="id",defaultValue = "1") String cafeid) {
+      return String.format("cafe id= %s!", cafeid);
+    }
+
+    @GetMapping("/cafe1")
+    public String cafeall(@RequestParam(value = "full", defaultValue = "1") String cafe) {
+      return String.format("cafe id: %s!", cafe);
+    }
+
+    @GetMapping("/msg1")
+    public String msg1(@RequestParam(value = "cmd", defaultValue = "echo") String cmd) {
+      return String.format("CMD %s!", cmd);
+    }
+
 }
